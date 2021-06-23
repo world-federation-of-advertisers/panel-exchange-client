98d8f59766df8439e45a20d9de661bb1b7069a56
Erin Freck <efreck@google.com>
2021-06-23 16:31:42 +0000

    Adds all the interfaces for encrypting user data
    Adds the interfaces aes, encrypt_identifier, encrypt_payload,
    fingerprinter, generate_hashkey, hkdf, and encrypt_original_data to be
    used for the encrypting user data.

---
+++ src/main/cc/wfanet/panelmatch/common/crypto/aes.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SRC_MAIN_CC_WFANET_PANELMATCH_COMMON_CRYPTO_AES_H_
+#define SRC_MAIN_CC_WFANET_PANELMATCH_COMMON_CRYPTO_AES_H_
+
+namespace wfanet::panelmatch::common::crypto {
+
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+
+class Aes {
+ public:
+  virtual ~Aes() = default;
+  virtual absl::StatusOr < std::string >>
+      Encrypt(absl::string_view input, absl::string_view key) = 0;
+  virtual absl::StatusOr < std::string >>
+      Decrypt(absl::string_view input, absl::string_view key) = 0;
+};
+
+}  // namespace wfanet::panelmatch::common::crypto
+
+#endif  // SRC_MAIN_CC_WFANET_PANELMATCH_COMMON_CRYPTO_AES_H_
