30ca107957b6fb635546baad45f15f7768bc7133
Erin Freck <efreck@google.com>
2021-06-25 14:48:15 +0000

    Added comments and changed interface names
    Changed the names of some of the interfaces as well as added comments
    for each class and function.

--- src/main/cc/wfanet/panelmatch/common/crypto/aes.h
+++ src/main/cc/wfanet/panelmatch/common/crypto/aes.h
@@ -19,14 +19,20 @@
 
 namespace wfanet::panelmatch::common::crypto {
 
+#include <string>
+
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 
+// An interface to ensure the Aes scheme used has an encrypt and decrypt method
+// that takes a key and an input
 class Aes {
  public:
   virtual ~Aes() = default;
+  // An encryption method that uses an AES Key to encrypt an input
   virtual absl::StatusOr<std::string> Encrypt(absl::string_view input,
                                               absl::string_view key) = 0;
+  // A decryption method that uses an AES Key to decrypt an input
   virtual absl::StatusOr<std::string> Decrypt(absl::string_view input,
                                               absl::string_view key) = 0;
 };
