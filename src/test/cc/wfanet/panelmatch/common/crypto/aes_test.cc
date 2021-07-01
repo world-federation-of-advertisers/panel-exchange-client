539a527d104ba58e739958911efc8f883056b1b0
Erin Freck <efreck@google.com>
2021-07-01 19:31:45 +0000

    Updated copyright comments
    Updated copyright comments

--- src/test/cc/wfanet/panelmatch/common/crypto/aes_test.cc
+++ src/test/cc/wfanet/panelmatch/common/crypto/aes_test.cc
@@ -1,3 +1,17 @@
+// Copyright 2021 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "wfanet/panelmatch/common/crypto/aes.h"
 
 #include <string>
@@ -6,9 +20,7 @@
 #include "src/test/cc/testutil/matchers.h"
 //#include "tink/util/secret_data.h"
 
-namespace crypto {
-namespace tink {
-namespace subtle {
+namespace wfanet::panelmatch::common::crypto {
 namespace {
 
 using ::crypto::tink::util::SecretDataFromStringView;
@@ -56,6 +68,4 @@ TEST(AesTest, unimplementedDecrypt) {
 // Same key with different strings are different things
 
 }  // namespace
-}  // namespace subtle
-}  // namespace tink
-}  // namespace crypto
+}  // namespace wfanet::panelmatch::common::crypto
