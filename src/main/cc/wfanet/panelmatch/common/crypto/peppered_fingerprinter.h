50e2ef481793b9638c6352032e7d0bc623e7719a
Erin Freck <efreck@google.com>
2021-07-08 16:36:48 +0000

    Fix Lint errors
    Fix Lint errors

--- src/main/cc/wfanet/panelmatch/common/crypto/peppered_fingerprinter.h
+++ src/main/cc/wfanet/panelmatch/common/crypto/peppered_fingerprinter.h
@@ -1,5 +1,23 @@
+/*
+ * Copyright 2021 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef SRC_MAIN_CC_WFANET_PANELMATCH_COMMON_CRYPTO_PEPPERED_FINGERPRINTER_H_
 #define SRC_MAIN_CC_WFANET_PANELMATCH_COMMON_CRYPTO_PEPPERED_FINGERPRINTER_H_
+
+#include <memory>
 
 #include "src/main/cc/any_sketch/fingerprinters/fingerprinters.h"
 #include "tink/util/secret_data.h"
@@ -15,4 +33,4 @@ std::unique_ptr<Fingerprinter> GetPepperedFingerprinter(
 
 }  // namespace wfanet::panelmatch::common::crypto
 
-#endif  // WFANET_PANELMATCH_COMMON_CRYPTO_PEPPERED_FINGERPRINTER_H_
+#endif  // SRC_MAIN_CC_WFANET_PANELMATCH_COMMON_CRYPTO_PEPPERED_FINGERPRINTER_H_
