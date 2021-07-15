0fd52e2fbf75e0940e10bbfcdbf728f043dce325
Jon Molle <jmolle@google.com>
2021-07-15 12:19:13 -0700

    Refactor cc packages to use matching namespaces
    wfanet -> wfa for cc, java/kotlin remain wfanet
    
    Also fixes
    * nhaarman/mockito -> official mockito in common-jvm. This either was
    rolled back or missed being changed in the common-jvm update PR.
    * some lint issues
    * removes some unnecessary duplicate imports

---
+++ src/main/cc/wfa/panelmatch/common/crypto/aes.cc
@@ -0,0 +1,45 @@
+// Copyright 2021 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "wfa/panelmatch/common/crypto/aes.h"
+
+#include "absl/status/status.h"
+
+namespace wfa::panelmatch::common::crypto {
+namespace {
+
+using ::crypto::tink::util::SecretData;
+
+// Implements an Aes SIV encryption scheme defined in
+// https://datatracker.ietf.org/doc/html/rfc5297
+class AesSiv : public Aes {
+ public:
+  AesSiv() = default;
+
+  absl::StatusOr<std::string> Encrypt(absl::string_view input,
+                                      const SecretData& key) const override {
+    return absl::UnimplementedError("Not implemented");
+  }
+
+  absl::StatusOr<std::string> Decrypt(absl::string_view input,
+                                      const SecretData& key) const override {
+    return absl::UnimplementedError("Not implemented");
+  }
+
+  int32_t key_size_bytes() const override { return 64; }
+};
+}  // namespace
+
+std::unique_ptr<Aes> GetAesSivCmac512() { return absl::make_unique<AesSiv>(); }
+}  // namespace wfa::panelmatch::common::crypto
