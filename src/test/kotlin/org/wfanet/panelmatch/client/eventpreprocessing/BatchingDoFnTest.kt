8e4be5c33b3fcf068aa1d2b48c991cf0b3724f17
Julia Morrissey <juliamorrissey@google.com>
2021-07-20 16:59:35 +0000

    Adds Teststream configuration and additional tests
    Creates and performs tests on fixed bundles using TestStream

--- src/test/kotlin/org/wfanet/panelmatch/client/eventpreprocessing/BatchingDoFnTest.kt
+++ src/test/kotlin/org/wfanet/panelmatch/client/eventpreprocessing/BatchingDoFnTest.kt
@@ -14,6 +14,8 @@
 
 package org.wfanet.panelmatch.client.eventpreprocessing
 
+import org.apache.beam.sdk.coders.StringUtf8Coder
+import org.apache.beam.sdk.testing.TestStream
 import org.apache.beam.sdk.transforms.DoFn
 import org.apache.beam.sdk.transforms.ParDo
 import org.apache.beam.sdk.transforms.SerializableFunction
@@ -29,19 +31,73 @@ import org.wfanet.panelmatch.common.beam.testing.assertThat
 @RunWith(JUnit4::class)
 class BatchingDoFnTest : BeamTestBase() {
 
-  private val collection: PCollection<String> by lazy {
-    pcollectionOf("collection1", "1", "2", "3")
-  }
-  private val collection2: PCollection<String> by lazy {
-    pcollectionOf("collection12", "1", "2", "345")
-  }
   @Test
   fun testBatching() {
-    val batchSize: Int = 1
-    val doFn: DoFn<String, MutableList<String>> = BatchingDoFn<String>(batchSize, StringLengthSize)
-    val result: PCollection<MutableList<String>> = collection.apply(ParDo.of(doFn))
-    assertThat(result)
+    val testStream1: TestStream<String> =
+      TestStream.create(StringUtf8Coder.of())
+        .addElements("1", "2", "3")
+        .advanceWatermarkToInfinity()
+    val batchSize1 = 1
+    val doFn1: DoFn<String, MutableList<String>> = BatchingDoFn(batchSize1, StringLengthSize)
+    var result1: PCollection<MutableList<String>>
+    pipeline.apply(testStream1).apply { result1 = this.apply(ParDo.of(doFn1)) }
+    pipeline.run()
+    assertThat(result1)
       .containsInAnyOrder(mutableListOf("1"), mutableListOf("2"), mutableListOf("3"))
+  }
+
+  @Test
+  fun testSingleBatch() {
+    val testStream2: TestStream<String> =
+      TestStream.create(StringUtf8Coder.of())
+        .addElements("1", "2", "3")
+        .advanceWatermarkToInfinity()
+    val batchSize2 = 3
+    val doFn2: DoFn<String, MutableList<String>> = BatchingDoFn(batchSize2, StringLengthSize)
+    var result2: PCollection<MutableList<String>>
+    pipeline.apply(testStream2).apply { result2 = this.apply(ParDo.of(doFn2)) }
+    pipeline.run()
+    assertThat(result2).containsInAnyOrder(mutableListOf("1", "2", "3"))
+  }
+
+  @Test
+  fun testMaxByteSizeElement() {
+    val testStream3: TestStream<String> =
+      TestStream.create(StringUtf8Coder.of())
+        .addElements("1", "234", "5")
+        .advanceWatermarkToInfinity()
+    val batchSize3 = 3
+    val doFn3: DoFn<String, MutableList<String>> = BatchingDoFn(batchSize3, StringLengthSize)
+    var result3: PCollection<MutableList<String>>
+    pipeline.apply(testStream3).apply { result3 = this.apply(ParDo.of(doFn3)) }
+    pipeline.run()
+    assertThat(result3).containsInAnyOrder(mutableListOf("1", "5"), mutableListOf("234"))
+  }
+
+  @Test
+  fun testExceedsMaxByteSizeElement() {
+    val testStream4: TestStream<String> =
+      TestStream.create(StringUtf8Coder.of())
+        .addElements("1", "234", "5")
+        .advanceWatermarkToInfinity()
+    val batchSize4 = 2
+    val doFn4: DoFn<String, MutableList<String>> = BatchingDoFn(batchSize4, StringLengthSize)
+    var result4: PCollection<MutableList<String>>
+    pipeline.apply(testStream4).apply { result4 = this.apply(ParDo.of(doFn4)) }
+    pipeline.run()
+    assertThat(result4).containsInAnyOrder(mutableListOf("1", "5"), mutableListOf("234"))
+  }
+
+  @Test
+  fun emptyElements() {
+    val testStream4: TestStream<String> =
+      TestStream.create(StringUtf8Coder.of()).advanceWatermarkToInfinity()
+    val batchSize4 = 3
+    val doFn4: DoFn<String, MutableList<String>> = BatchingDoFn(batchSize4, StringLengthSize)
+    var result4: PCollection<MutableList<String>>
+    pipeline.apply(testStream4).apply { result4 = this.apply(ParDo.of(doFn4)) }
+    pipeline.run()
+    assertThat(result4).empty()
   }
 }
 
