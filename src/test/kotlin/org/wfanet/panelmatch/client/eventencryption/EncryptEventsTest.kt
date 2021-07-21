7a5b452b361f8f8853c977c7af4f454165d81eb2
Julia Morrissey <juliamorrissey@google.com>
2021-07-21 20:48:50 +0000

    Creates EncryptEvents
    Creates implementation for encryption serializable function and tests
    that it returns kUnimplemented for now

---
+++ src/test/kotlin/org/wfanet/panelmatch/client/eventencryption/EncryptEventsTest.kt
@@ -0,0 +1,48 @@
+// Copyright 2021 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.panelmatch.client.eventencryption
+
+import com.google.common.truth.Truth.assertThat
+import com.google.protobuf.ByteString
+import kotlin.test.assertFailsWith
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.wfanet.panelmatch.client.PreprocessEventsRequest
+import org.wfanet.panelmatch.client.PreprocessEventsResponse
+import org.wfanet.panelmatch.common.JniException
+
+@RunWith(JUnit4::class)
+class EncryptEventsTest {
+
+  @Test
+  fun kUnimplementedStatus() {
+    val unImplemented =
+      assertFailsWith(JniException::class) {
+        val request =
+          PreprocessEventsRequest.newBuilder()
+            .apply {
+              addUnprocessedEventsBuilder().apply {
+                this.id = ByteString.copyFromUtf8("11111")
+                this.data = ByteString.copyFromUtf8("11111")
+              }
+            }
+            .build()
+        val E: EncryptEvents = EncryptEvents()
+        val response: PreprocessEventsResponse = E.apply(request)
+      }
+    assertThat(unImplemented.message).contains("UNIMPLEMENTED: Not implemented")
+  }
+}
