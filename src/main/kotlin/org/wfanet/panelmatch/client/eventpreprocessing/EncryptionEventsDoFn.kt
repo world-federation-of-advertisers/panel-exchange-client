5e6336ea0931a1166d6939f61234ee2d361d20d2
Julia Morrissey <juliamorrissey@google.com>
2021-07-23 21:41:25 +0000

    Resolve Merge Conflicts

--- src/main/kotlin/org/wfanet/panelmatch/client/eventpreprocessing/EncryptionEventsDoFn.kt
+++ src/main/kotlin/org/wfanet/panelmatch/client/eventpreprocessing/EncryptionEventsDoFn.kt
@@ -31,7 +31,7 @@ class EncryptionEventsDoFn(
     SerializableFunction<PreprocessEventsRequest, PreprocessEventsResponse>,
   private val getPepper: SerializableFunction<Void?, ByteString>,
   private val getCryptoKey: SerializableFunction<Void?, ByteString>,
-) : DoFn<MutableList<KV<ByteString, ByteString>>, KV<ByteString, ByteString>>() {
+) : DoFn<MutableList<KV<ByteString, ByteString>>, KV<Long, ByteString>>() {
   @ProcessElement
   fun process(c: ProcessContext) {
     val list: MutableList<KV<ByteString, ByteString>> = c.element()
