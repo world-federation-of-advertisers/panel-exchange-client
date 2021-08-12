ac4cf7713288eafd11a306a065055f83021c3fc2
Erin Freck <efreck@google.com>
2021-08-12 21:25:35 +0000

    Fix Lint errors
    Fix Lint errors

--- src/main/cc/wfa/panelmatch/client/eventpreprocessing/preprocess_events.cc
+++ src/main/cc/wfa/panelmatch/client/eventpreprocessing/preprocess_events.cc
@@ -60,7 +60,7 @@ PreprocessEvents(
   if (request.pepper().empty()) {
     return absl::InvalidArgumentError("INVALID ARGUMENT: Empty Pepper");
   }
-  // TODO: load the salt from the request
+  // TODO(juliamorrissey): load the salt from the request
   EventDataPreprocessor preprocessor = EventDataPreprocessor(
       std::move(cryptor), SecretDataFromStringView(request.pepper()),
       SecretDataFromStringView(""), &fingerprinter, &aes_hkdf);
