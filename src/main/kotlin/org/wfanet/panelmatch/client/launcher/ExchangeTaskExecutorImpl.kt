b9bf9db5bbdde8588f0fb02f1a5e3eb15cd6bb4f
Jon Molle <jmolle@google.com>
2021-08-16 11:48:12 -0700

    Refactored out the Storage class
    - This moves to using the underlying StorageClient classes directly.
    - functions like batchRead/Write have been made helpers, with the intent
    to migrate them into StorageClient as-needed.
    - Added a stub function for each verifiedRead/Write, with a downstream
    PR to implement the new signing/verification code (as well as a PR to
    move some of that code's dependencies into common- repos.)
    - Deletes: InMemoryStorage, Storage (class)
    - FileSystemStorage remains due to the init code not having a place to
    migrate yet. Once that has a home in either a constructor for
    StorageClient or init function for exchange steps.
    
    unit tests refactored to cover the new code.

--- src/main/kotlin/org/wfanet/panelmatch/client/launcher/ExchangeTaskExecutorImpl.kt
+++ src/main/kotlin/org/wfanet/panelmatch/client/launcher/ExchangeTaskExecutorImpl.kt
@@ -25,12 +25,14 @@ import kotlinx.coroutines.withContext
 import org.wfanet.measurement.api.v2alpha.ExchangeStepAttempt
 import org.wfanet.measurement.api.v2alpha.ExchangeStepAttemptKey
 import org.wfanet.measurement.api.v2alpha.ExchangeWorkflow
+import org.wfanet.measurement.storage.StorageClient
 import org.wfanet.measurement.storage.StorageClient.Blob
 import org.wfanet.panelmatch.client.exchangetasks.ExchangeTask
 import org.wfanet.panelmatch.client.logger.addToTaskLog
 import org.wfanet.panelmatch.client.logger.getAndClearTaskLog
 import org.wfanet.panelmatch.client.logger.loggerFor
-import org.wfanet.panelmatch.client.storage.Storage
+import org.wfanet.panelmatch.client.storage.batchRead
+import org.wfanet.panelmatch.client.storage.batchWrite
 import org.wfanet.panelmatch.common.Timeout
 
 /**
@@ -40,8 +42,8 @@ import org.wfanet.panelmatch.common.Timeout
 class ExchangeTaskExecutorImpl(
   private val apiClient: ApiClient,
   private val timeout: Timeout,
-  private val sharedStorage: Storage,
-  private val privateStorage: Storage,
+  private val sharedStorage: StorageClient,
+  private val privateStorage: StorageClient,
   private val getExchangeTaskForStep: suspend (ExchangeWorkflow.Step) -> ExchangeTask
 ) : ExchangeStepExecutor {
   /** Reads inputs for [step], executes [step], and writes the outputs to appropriate [Storage]. */
