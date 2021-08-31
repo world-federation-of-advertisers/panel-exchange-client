0302138049eedecf847a8536838a97e17c99af5f
Steven Ware Jones <stevenwjones@fb.com>
2021-08-31 18:24:52 -0400

    address requested changes

--- src/main/kotlin/org/wfanet/panelmatch/common/crypto/testing/ConcatSymmetricCryptor.kt
+++ src/main/kotlin/org/wfanet/panelmatch/common/crypto/testing/ConcatSymmetricCryptor.kt
@@ -21,12 +21,19 @@ import org.wfanet.panelmatch.common.toByteString
 /** Does no real crypto. It only xors [data] with [privateKey]. */
 class ConcatSymmetricCryptor : SymmetricCryptor {
 
+  private val SEPARATOR = " encrypted by "
   override fun encrypt(privateKey: ByteString, data: ByteString): ByteString {
-    return privateKey.concat(data)
+    return data.concat(SEPARATOR.toByteString()).concat(privateKey)
   }
 
   override fun decrypt(privateKey: ByteString, data: ByteString): ByteString {
-    require(data.toStringUtf8().startsWith(privateKey.toStringUtf8()))
-    return data.toStringUtf8().substring(privateKey.toStringUtf8().length).toByteString()
+    require(data.toStringUtf8().endsWith(privateKey.toStringUtf8()))
+    return data
+      .toStringUtf8()
+      .substring(
+        0,
+        data.toStringUtf8().length - privateKey.toStringUtf8().length - SEPARATOR.length
+      )
+      .toByteString()
   }
 }
