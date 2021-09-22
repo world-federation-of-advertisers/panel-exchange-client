307cb6b851e34c812f140d94f4158f15560408c9
Eli Fox-Epstein <efoxepstein@google.com>
2021-09-22 17:31:25 +0000

    Add WorkflowExchangeTask to run Beam pipelines

---
+++ src/main/kotlin/org/wfanet/panelmatch/client/exchangetasks/WorkflowExchangeTask.kt
@@ -0,0 +1,51 @@
+// Copyright 2021 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.panelmatch.client.exchangetasks
+
+import com.google.protobuf.ByteString
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import org.wfanet.measurement.common.flatten
+import org.wfanet.panelmatch.client.storage.VerifiedStorageClient.VerifiedBlob
+import org.wfanet.panelmatch.common.toByteString
+
+/** Executes workflows (e.g. Apache Beam pipelines) in an [ExchangeTask]. */
+class WorkflowExchangeTask(private val workflow: Workflow) : ExchangeTask {
+
+  /** Abstraction over a workflow. */
+  interface Workflow {
+    /**
+     * Executes an abstract workflow.
+     *
+     * @param inputUris a map from input name to a URI pattern where it can be read from
+     * @return a map from output name to a URI pattern where it was written to
+     */
+    suspend fun execute(inputUris: Map<String, String>): Map<String, String>
+  }
+
+  override suspend fun execute(input: Map<String, VerifiedBlob>): Map<String, Flow<ByteString>> {
+    val inputUris = input.mapValues { makeInput(it.value) }
+    val outputUris = workflow.execute(inputUris)
+    return outputUris.mapValues { makeOutputBlob(it.value) }
+  }
+}
+
+private suspend fun makeInput(blob: VerifiedBlob): String {
+  return blob.read().flatten().toStringUtf8()
+}
+
+private fun makeOutputBlob(uri: String): Flow<ByteString> {
+  return flowOf(uri.toByteString())
+}
