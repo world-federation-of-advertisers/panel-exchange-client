f7bf6239017d90ade1ad035a270182bcc03739d3
Eli Fox-Epstein <efoxepstein@google.com>
2021-09-29 20:26:27 +0000

    Experiment: load test Private Membership query creation and execution

---
+++ src/main/kotlin/org/wfanet/panelmatch/client/exchangetasks/ModelProviderApacheBeamTask.kt
@@ -0,0 +1,36 @@
+// Copyright 2021 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.panelmatch.client.exchangetasks
+
+import org.apache.beam.sdk.values.PCollectionView
+import org.wfanet.panelmatch.client.privatemembership.PrivateMembershipKeys
+import org.wfanet.panelmatch.common.beam.mapWithSideInput
+import org.wfanet.panelmatch.common.beam.toSingletonView
+
+abstract class ModelProviderApacheBeamTask : ApacheBeamTask() {
+  val privateMembershipKeys: PCollectionView<PrivateMembershipKeys> by lazy {
+    val privateKeys =
+      readFileAsSingletonPCollection("rlwe-serialized-private-key", localCertificate)
+    val publicKeyView =
+      readFileAsSingletonPCollection("rlwe-serialized-public-key", localCertificate)
+        .toSingletonView()
+
+    privateKeys
+      .mapWithSideInput(publicKeyView, "Make PrivateMembershipKeys") { privateKey, publicKey ->
+        PrivateMembershipKeys(serializedPublicKey = publicKey, serializedPrivateKey = privateKey)
+      }
+      .toSingletonView()
+  }
+}
