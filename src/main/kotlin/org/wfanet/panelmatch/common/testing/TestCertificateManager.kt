1fcb3305e3b073b4be42a1d70b8a67d9e958d924
Jon Molle <jmolle@google.com>
2021-10-07 23:23:26 +0000

    Implement a skeleton of certificateManager

--- src/main/kotlin/org/wfanet/panelmatch/common/testing/TestCertificateManager.kt
+++ src/main/kotlin/org/wfanet/panelmatch/common/testing/TestCertificateManager.kt
@@ -14,27 +14,31 @@
 
 package org.wfanet.panelmatch.common.testing
 
+import java.security.PrivateKey
 import java.security.cert.X509Certificate
 import org.wfanet.measurement.api.v2alpha.ExchangeKey
 import org.wfanet.measurement.common.crypto.readCertificate
+import org.wfanet.measurement.common.crypto.readPrivateKey
 import org.wfanet.measurement.common.crypto.testing.FIXED_SERVER_CERT_PEM_FILE
+import org.wfanet.measurement.common.crypto.testing.FIXED_SERVER_KEY_FILE
+import org.wfanet.measurement.common.crypto.testing.KEY_ALGORITHM
 import org.wfanet.panelmatch.common.CertificateManager
 
 class TestCertificateManager : CertificateManager {
-  override suspend fun getOwnedExchangeCertificate(
+
+  override suspend fun getCertificate(
     exchangeKey: ExchangeKey,
-  ): X509Certificate {
+    certOwnerName: String,
+    certResourceName: String
+  ): Pair<X509Certificate, String> {
+    return (readCertificate(FIXED_SERVER_CERT_PEM_FILE) to certResourceName)
+  }
+
+  override suspend fun getPartnerRootCertificate(partnerName: String): X509Certificate {
     return readCertificate(FIXED_SERVER_CERT_PEM_FILE)
   }
 
-  override suspend fun getPartnerExchangeCertificate(
-    exchangeKey: ExchangeKey,
-    partnerName: String,
-  ): X509Certificate {
-    return readCertificate(FIXED_SERVER_CERT_PEM_FILE)
-  }
-
-  override suspend fun getPartnerExchangeRootCertificate(partnerName: String): X509Certificate {
-    return readCertificate(FIXED_SERVER_CERT_PEM_FILE)
+  override fun getExchangePrivateKey(exchangeKey: ExchangeKey): PrivateKey {
+    return readPrivateKey(FIXED_SERVER_KEY_FILE, KEY_ALGORITHM)
   }
 }
