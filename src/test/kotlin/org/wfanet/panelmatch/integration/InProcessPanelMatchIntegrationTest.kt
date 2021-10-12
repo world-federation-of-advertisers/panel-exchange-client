af28e523539b079b787206177d2667248144c585
yunyeng <yunyeng@google.com>
2021-10-12 23:10:25 +0000

    add main test

--- src/test/kotlin/org/wfanet/panelmatch/integration/InProcessPanelMatchIntegrationTest.kt
+++ src/test/kotlin/org/wfanet/panelmatch/integration/InProcessPanelMatchIntegrationTest.kt
@@ -15,7 +15,9 @@
 package org.wfanet.panelmatch.integration
 
 import com.google.type.Date
-import io.grpc.ManagedChannel
+import io.grpc.Metadata
+import io.grpc.stub.AbstractStub
+import io.grpc.stub.MetadataUtils
 import java.lang.IllegalArgumentException
 import java.time.Clock
 import java.time.Instant
@@ -23,15 +25,23 @@ import java.time.LocalDate
 import java.time.ZoneOffset
 import java.util.logging.Logger
 import kotlinx.coroutines.runBlocking
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestRule
+import org.wfanet.measurement.api.v2alpha.DataProviderKey
 import org.wfanet.measurement.api.v2alpha.ExchangeStepAttempt
 import org.wfanet.measurement.api.v2alpha.ExchangeStepAttemptKey
 import org.wfanet.measurement.api.v2alpha.ExchangeStepAttemptsGrpcKt.ExchangeStepAttemptsCoroutineStub
 import org.wfanet.measurement.api.v2alpha.ExchangeStepsGrpcKt.ExchangeStepsCoroutineStub
 import org.wfanet.measurement.api.v2alpha.ExchangeWorkflow
+import org.wfanet.measurement.api.v2alpha.ModelProviderKey
+import org.wfanet.measurement.common.identity.apiIdToExternalId
+import org.wfanet.measurement.common.identity.withPrincipalName
 import org.wfanet.measurement.common.parseTextProto
+import org.wfanet.measurement.common.testing.chainRulesSequentially
 import org.wfanet.measurement.common.toProtoDate
-import org.wfanet.measurement.kingdom.service.internal.testing.integration.PanelMatchResourceSetup
+import org.wfanet.measurement.integration.deploy.gcloud.buildKingdomSpannerEmulatorDatabaseRule
+import org.wfanet.measurement.integration.deploy.gcloud.buildSpannerInProcessKingdom
 import org.wfanet.panelmatch.client.launcher.ApiClient.ClaimedExchangeStep
 import org.wfanet.panelmatch.client.launcher.GrpcApiClient
 import org.wfanet.panelmatch.client.launcher.Identity
@@ -44,27 +54,42 @@ private const val DATA_PROVIDER_ID = "some-data-provider-id"
 private val DATA_PROVIDER_IDENTITY =
   Identity(DATA_PROVIDER_ID, ExchangeWorkflow.Party.DATA_PROVIDER)
 
-class InProcessPanelMatchIntegrationTest(
-  private val internalApiChannel: ManagedChannel,
-  private val publicApiChannel: ManagedChannel,
-  private val clock: Clock = Clock.fixed(Instant.ofEpochSecond(123456789), ZoneOffset.UTC),
-  private val verboseGrpcLogging: Boolean = true
-) {
-  private val resourceSetup by lazy { PanelMatchResourceSetup(internalApiChannel) }
+class InProcessPanelMatchIntegrationTest {
+  private val clock: Clock = Clock.fixed(Instant.ofEpochSecond(123456789), ZoneOffset.UTC)
 
-  private val apiClient by lazy {
-    GrpcApiClient(
-      DATA_PROVIDER_IDENTITY,
-      ExchangeStepsCoroutineStub(publicApiChannel),
-      ExchangeStepAttemptsCoroutineStub(publicApiChannel),
+  val databaseRule = buildKingdomSpannerEmulatorDatabaseRule()
+  val inProcessKingdom = buildSpannerInProcessKingdom(databaseRule, clock)
+  //
+  //  private val apiClient by lazy {
+  //    GrpcApiClient(
+  //      DATA_PROVIDER_IDENTITY,
+  //      ExchangeStepsCoroutineStub(inProcessKingdom.publicApiChannel),
+  //      ExchangeStepAttemptsCoroutineStub(inProcessKingdom.publicApiChannel),
+  //      clock
+  //    )
+  //  }
+
+  private fun makeClient(identity: Identity, principalName: String): GrpcApiClient {
+    return GrpcApiClient(
+      identity,
+      ExchangeStepsCoroutineStub(inProcessKingdom.publicApiChannel).withPrincipalName(principalName),
+      ExchangeStepAttemptsCoroutineStub(inProcessKingdom.publicApiChannel).withPrincipalName(principalName),
       clock
     )
   }
+
+  @get:Rule
+  val ruleChain: TestRule by lazy { chainRulesSequentially(databaseRule, inProcessKingdom) }
+
+  val resourceSetup = inProcessKingdom.panelMatchResourceSetup
 
   private lateinit var dataProviderKey: String
   private lateinit var modelProviderKey: String
 
   private suspend fun setup() {
+
+    //    val principal = Principal.DataProvider(DataProviderKey(externalIdToApiId(12345L)))
+
     val providers =
       resourceSetup.createResourcesForWorkflow(
         exchangeSchedule = SCHEDULE,
@@ -80,34 +105,42 @@ class InProcessPanelMatchIntegrationTest(
   @Test
   fun `entire process`() = runBlocking {
     setup()
-    logger.addToTaskLog(
-      "Setup complete by creating a MP ${modelProviderKey} and an EDP ${dataProviderKey}."
-    )
+//    logger.addToTaskLog(
+//      "Setup complete by creating a MP ${modelProviderKey} and an EDP ${dataProviderKey}."
+//    )
 
-    var numberOfJobs = 0
-    while (true) {
-      val job = claimJob()
-      logger.addToTaskLog("Claimed a job with Step Index: ${job.exchangeStep.stepIndex}.")
-      numberOfJobs++
+    val dataProviderIdentity =
+      Identity(DataProviderKey.fromName(dataProviderKey)!!.dataProviderId, ExchangeWorkflow.Party.DATA_PROVIDER)
+    val modelProviderIdentity =
+      Identity(ModelProviderKey.fromName(modelProviderKey)!!.modelProviderId, ExchangeWorkflow.Party.MODEL_PROVIDER)
+    val apiClient = makeClient(modelProviderIdentity, modelProviderKey)
+    val job = apiClient.claimJob()
+    println(job)
 
-      finishJob(job.exchangeStepAttempt, ExchangeStepAttempt.State.SUCCEEDED)
-      logger.addToTaskLog(
-        "Finished the job with Step Index: ${job.exchangeStep.stepIndex} successfully."
-      )
-    }
+//    var numberOfJobs = 0
+//    while (true) {
+//      val job = apiClient.claimJob()
+////      logger.addToTaskLog("Claimed a job with Step Index: ${job.exchangeStep.stepIndex}.")
+//      numberOfJobs++
+//
+//      apiClient.finishJob(job.exchangeStepAttempt, ExchangeStepAttempt.State.SUCCEEDED)
+////      logger.addToTaskLog(
+////        "Finished the job with Step Index: ${job.exchangeStep.stepIndex} successfully."
+////      )
+//    }
 
-//    assertThat(numberOfJobs).isEqualTo(7)
+    //    assertThat(numberOfJobs).isEqualTo(7)
   }
 
-  private suspend fun claimJob(): ClaimedExchangeStep {
-    return apiClient.claimExchangeStep() ?: throw IllegalArgumentException("Couldn't find any job.")
+  private suspend fun GrpcApiClient.claimJob(): ClaimedExchangeStep {
+    return claimExchangeStep() ?: throw IllegalArgumentException("Couldn't find any job.")
   }
 
-  private suspend fun finishJob(
+  private suspend fun GrpcApiClient.finishJob(
     attemptKey: ExchangeStepAttemptKey,
     state: ExchangeStepAttempt.State
   ) {
-    apiClient.finishExchangeStepAttempt(
+    finishExchangeStepAttempt(
       key = attemptKey,
       finalState = state,
       logEntryMessages = listOfNotNull("")
