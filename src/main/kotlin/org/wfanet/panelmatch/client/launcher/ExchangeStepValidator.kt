5753c40590e06dac8df575a39d0193573ad34f59
Eli Fox-Epstein <efoxepstein@google.com>
2021-10-13 22:08:04 +0000

    Verify that Exchanges have valid dates.

--- src/main/kotlin/org/wfanet/panelmatch/client/launcher/ExchangeStepValidator.kt
+++ src/main/kotlin/org/wfanet/panelmatch/client/launcher/ExchangeStepValidator.kt
@@ -14,18 +14,29 @@
 
 package org.wfanet.panelmatch.client.launcher
 
+import com.google.protobuf.InvalidProtocolBufferException
+import java.time.Clock
 import org.wfanet.measurement.api.v2alpha.ExchangeStep
 import org.wfanet.measurement.api.v2alpha.ExchangeStepKey
 import org.wfanet.measurement.api.v2alpha.ExchangeWorkflow
+import org.wfanet.measurement.common.toLocalDate
+import org.wfanet.panelmatch.client.launcher.InvalidExchangeStepException.FailureType.PERMANENT
+import org.wfanet.panelmatch.client.launcher.InvalidExchangeStepException.FailureType.TRANSIENT
 import org.wfanet.panelmatch.common.secrets.SecretMap
 
 /** Indicates that an [ExchangeStep] is not valid to execute. */
-class InvalidExchangeStepException(message: String) : Exception(message)
+class InvalidExchangeStepException(val type: FailureType, message: String) : Exception(message) {
+  enum class FailureType {
+    PERMANENT,
+    TRANSIENT
+  }
+}
 
 /** Determines whether an ExchangeStep is valid and can be safely executed. */
 class ExchangeStepValidator(
   private val party: ExchangeWorkflow.Party,
-  private val validExchangeWorkflows: SecretMap
+  private val validExchangeWorkflows: SecretMap,
+  private val clock: Clock
 ) {
   /** Throws [InvalidExchangeStepException] if [exchangeStep] is invalid. */
   suspend fun validate(exchangeStep: ExchangeStep) {
@@ -33,14 +44,41 @@ class ExchangeStepValidator(
     val recurringExchangeId =
       requireNotNull(ExchangeStepKey.fromName(exchangeStep.name)).recurringExchangeId
     if (validExchangeWorkflows.get(recurringExchangeId) != serializedExchangeWorkflow) {
-      throw InvalidExchangeStepException("Serialized ExchangeWorkflow unrecognized")
+      throw InvalidExchangeStepException(PERMANENT, "Serialized ExchangeWorkflow unrecognized")
     }
 
     @Suppress("BlockingMethodInNonBlockingContext") // Proto parsing is lightweight
-    val workflow = ExchangeWorkflow.parseFrom(serializedExchangeWorkflow)
-    val step = workflow.getSteps(exchangeStep.stepIndex)
+    val workflow =
+      try {
+        ExchangeWorkflow.parseFrom(serializedExchangeWorkflow)
+      } catch (e: InvalidProtocolBufferException) {
+        throw InvalidExchangeStepException(PERMANENT, "Invalid ExchangeWorkflow")
+      }
+
+    val stepIndex = exchangeStep.stepIndex
+    if (stepIndex !in 0 until workflow.stepsCount) {
+      throw InvalidExchangeStepException(PERMANENT, "Invalid step_index: $stepIndex")
+    }
+    val step = workflow.getSteps(stepIndex)
     if (step.party != party) {
-      throw InvalidExchangeStepException("Party for step '${step.stepId}' was not ${party.name}")
+      throw InvalidExchangeStepException(
+        PERMANENT,
+        "Party for step '${step.stepId}' was not ${party.name}"
+      )
+    }
+
+    val firstExchangeDate = workflow.firstExchangeDate.toLocalDate()
+    val exchangeDate = exchangeStep.exchangeDate.toLocalDate()
+    if (exchangeDate < firstExchangeDate) {
+      throw InvalidExchangeStepException(
+        PERMANENT,
+        "exchange_date is before ExchangeWorkflow.first_exchange_date"
+      )
+    }
+
+    val exchangeTime = exchangeDate.atStartOfDay(clock.zone).toInstant()
+    if (exchangeTime > clock.instant()) {
+      throw InvalidExchangeStepException(TRANSIENT, "exchange_date is in the future")
     }
   }
 }
