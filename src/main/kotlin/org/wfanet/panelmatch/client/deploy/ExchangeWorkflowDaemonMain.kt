7c21a40946a6c43df1d3867e5c2173733d87d8bc
Jon Molle <jmolle@google.com>
2021-10-25 21:45:56 +0000

    Build StorageSelectors in ExchangeWorkflowDaemon.
    Co-authored-by: Jon Molle <jmolle@google.com>

--- src/main/kotlin/org/wfanet/panelmatch/client/deploy/ExchangeWorkflowDaemonMain.kt
+++ src/main/kotlin/org/wfanet/panelmatch/client/deploy/ExchangeWorkflowDaemonMain.kt
@@ -16,10 +16,15 @@ package org.wfanet.panelmatch.client.deploy
 
 import java.time.Clock
 import org.wfanet.measurement.common.commandLineMain
-import org.wfanet.panelmatch.client.storage.PrivateStorageSelector
-import org.wfanet.panelmatch.client.storage.SharedStorageSelector
-import org.wfanet.panelmatch.client.storage.VerifiedStorageClient
-import org.wfanet.panelmatch.common.certificates.CertificateManager
+import org.wfanet.panelmatch.client.common.ExchangeContext
+import org.wfanet.panelmatch.client.storage.FileSystemStorageFactory
+import org.wfanet.panelmatch.client.storage.GcsStorageFactory
+import org.wfanet.panelmatch.client.storage.StorageDetails
+import org.wfanet.panelmatch.client.storage.StorageDetails.PlatformCase
+import org.wfanet.panelmatch.client.storage.StorageFactory
+import org.wfanet.panelmatch.common.ExchangeDateKey
+import org.wfanet.panelmatch.common.certificates.CertificateAuthority
+import org.wfanet.panelmatch.common.secrets.MutableSecretMap
 import org.wfanet.panelmatch.common.secrets.SecretMap
 import picocli.CommandLine
 
@@ -38,25 +43,48 @@ private object UnimplementedExchangeWorkflowDaemon : ExchangeWorkflowDaemonFromF
   lateinit var blobSizeFlags: BlobSizeFlags
     private set
 
-  override val certificateManager: CertificateManager
+  // TODO: All SecretMaps should be implemented with a static storageClient- or KMS-backed SecretMap
+  //  based on flags. To start this should support our existing StorageFactory implementations
+  //  and call a helper function to choose based on storage details (similar to
+  //  privateStorageSelector, which we unfortunately can't use here as it depends on some of these.)
+  override val privateKeys: MutableSecretMap
     get() = TODO("Not yet implemented")
 
-  override val privateStorageSelector: PrivateStorageSelector
+  override val rootCertificates: SecretMap
     get() = TODO("Not yet implemented")
 
-  override val sharedStorageSelector: SharedStorageSelector
+  // This is the most likely piece of storage to be customized, as it gives a party control over
+  // what StorageClient types they actually support for shared and private storage. That's the main
+  // reason it has been pushed all the way out here for implementation: we expect most custom main
+  // file implementations to want to specify these.
+  override val privateStorageFactories:
+    Map<PlatformCase, ExchangeDateKey.(StorageDetails) -> StorageFactory> =
+    mapOf(PlatformCase.FILE to { storageDetails -> FileSystemStorageFactory(storageDetails, this) })
+
+  override val privateStorageInfo: SecretMap
+    get() = TODO("Not yet implemented")
+
+  override val sharedStorageFactories:
+    Map<PlatformCase, ExchangeContext.(StorageDetails) -> StorageFactory> =
+    mapOf(
+      PlatformCase.GCS to
+        { storageDetails ->
+          blobSizeFlags.wrapStorageFactory(GcsStorageFactory(storageDetails, exchangeDateKey))
+        }
+    )
+
+  override val sharedStorageInfo: SecretMap
     get() = TODO("Not yet implemented")
 
   override val clock: Clock = Clock.systemUTC()
+
+  override val certificateAuthority: CertificateAuthority
+    get() = TODO("Not yet implemented")
 
   override val validExchangeWorkflows: SecretMap by lazy {
     approvedWorkflowFlags.approvedExchangeWorkflows
   }
 }
 
-/**
- * Reference implementation of a daemon for executing Exchange Workflows.
- *
- * TODO(@jonmolle): implement the proper [VerifiedStorageClient]s and any flags to support them.
- */
+/** Reference implementation of a daemon for executing Exchange Workflows. */
 fun main(args: Array<String>) = commandLineMain(UnimplementedExchangeWorkflowDaemon, args)
