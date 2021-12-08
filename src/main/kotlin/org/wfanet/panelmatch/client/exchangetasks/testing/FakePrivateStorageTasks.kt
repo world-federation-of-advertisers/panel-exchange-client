fae972eda2adcb00f66ed00ac86b41c2a95ef958
yunyeng <yunyeng@google.com>
2021-12-08 17:50:53 +0000

    use fake exchange task mapper

--- src/main/kotlin/org/wfanet/panelmatch/client/exchangetasks/testing/FakePrivateStorageTasks.kt
+++ src/main/kotlin/org/wfanet/panelmatch/client/exchangetasks/testing/FakePrivateStorageTasks.kt
@@ -18,7 +18,7 @@ import org.wfanet.panelmatch.client.common.ExchangeContext
 import org.wfanet.panelmatch.client.exchangetasks.PrivateStorageTasks
 
 class FakePrivateStorageTasks : PrivateStorageTasks {
-  override suspend fun getInput(context: ExchangeContext) = FakeExchangeTask("get-input")
+  override suspend fun input(context: ExchangeContext) = FakeInputTask("get-input")
   override suspend fun copyFromPreviousExchange(context: ExchangeContext) =
     FakeExchangeTask("copy-from-prev-exchange")
 }
