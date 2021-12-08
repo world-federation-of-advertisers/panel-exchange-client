a80b41c067df1d602cdd23a8d1f15ba8d17d2a63
yunyeng <yunyeng@google.com>
2021-12-08 18:15:38 +0000

    rename makeFakeExchangeTaskMapper

--- src/main/kotlin/org/wfanet/panelmatch/client/exchangetasks/testing/FakeCommutativeEncryptionTasks.kt
+++ src/main/kotlin/org/wfanet/panelmatch/client/exchangetasks/testing/FakeCommutativeEncryptionTasks.kt
@@ -17,8 +17,8 @@ package org.wfanet.panelmatch.client.exchangetasks.testing
 import org.wfanet.panelmatch.client.exchangetasks.CommutativeEncryptionTasks
 
 class FakeCommutativeEncryptionTasks : CommutativeEncryptionTasks {
-  override fun encrypt() = FakeCryptorExchangeTask("encrypt")
+  override fun encrypt() = FakeExchangeTask("encrypt")
   override fun reEncrypt() = FakeExchangeTask("re-encrypt")
   override fun decrypt() = FakeExchangeTask("decrypt")
-  override fun generateCommutativeEncryptionKey() = FakeExchangeTask("generate-encryption-key")
+  override fun generateEncryptionKey() = FakeExchangeTask("generate-encryption-key")
 }
