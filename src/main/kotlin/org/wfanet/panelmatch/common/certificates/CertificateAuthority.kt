d5c8a1b58f2e1bc798dbbbb824f340e08443f3d4
mpatel24 <mahitosh24@gmail.com>
2021-12-13 20:35:56 -0500

    linting

--- src/main/kotlin/org/wfanet/panelmatch/common/certificates/CertificateAuthority.kt
+++ src/main/kotlin/org/wfanet/panelmatch/common/certificates/CertificateAuthority.kt
@@ -28,7 +28,6 @@ interface CertificateAuthority {
   )
 
   /**
-   *
    * Creates a [PrivateKey] and corresponding [X509Certificate] signed by a root private key and
    * verifiable by the shared root public key.
    *
