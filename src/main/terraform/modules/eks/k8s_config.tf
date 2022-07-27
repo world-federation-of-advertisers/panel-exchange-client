dcf288b869404702960d8417c3027c6feb2ba576
Brandon Domen <bdomen@google.com>
2022-07-27 18:37:40 +0000

    initial terraform setup

---
+++ src/main/terraform/modules/eks/k8s_config.tf
@@ -0,0 +1,24 @@
+resource "null_resource" "collect_k8s_secrets" {
+  provisioner "local-exec" {
+    command = <<EOF
+cp -r ${var.path_to_cmm}/src/main/k8s/testing/secretfiles ${var.path_to_secrets}
+
+cat ${var.path_to_secrets}/*_root.pem > ${var.path_to_secrets}/all_root_certs.pem
+    EOF
+  }
+}
+
+resource "null_resource" "create_k8s_secrets2" {
+  provisioner "local-exec" {
+    command = <<EOF
+echo "secretGenerator:" > ${var.path_to_secrets}/kustomization.yaml
+echo "- name: certs-and-configs" >> ${var.path_to_secrets}/kustomization.yaml
+echo "  Files:" >> ${var.path_to_secrets}/kustomization.yaml
+#echo "  - trusted_certs.pem" >> ${var.path_to_secrets}/kustomization.yaml
+echo "  - edp1_tls.pem" >> ${var.path_to_secrets}/kustomization.yaml
+echo "  - edp1_tls.key" >> ${var.path_to_secrets}/kustomization.yaml
+
+kubectl apply -k ${var.path_to_secrets}
+    EOF
+  }
+}
