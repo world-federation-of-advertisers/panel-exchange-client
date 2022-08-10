c7d39d72cd67f934ff08bf04a55359e2753ab8a9
Brandon Domen <bdomen@google.com>
2022-08-10 14:57:00 +0000

    forgot to update main.tf file

--- src/main/terraform/main.tf
+++ src/main/terraform/main.tf
@@ -6,3 +6,8 @@ module "docker_config" {
   depends_on = [module.aws_eks_cluster]
   source = "./modules/docker"
 }
+
+module "other_resources" {
+  depends_on = [module.docker_config]
+  source = "./modules/other"
+}
