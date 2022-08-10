d33524b61d50f447523af53d250df7e43a3b1e9b
Brandon Domen <bdomen@google.com>
2022-08-10 15:00:10 +0000

    more updates

--- src/main/terraform/modules/other/kms.tf
+++ src/main/terraform/modules/other/kms.tf
@@ -2,3 +2,8 @@ resource "aws_kms_key" "my_key" {
   description             = "key for ocmm"
   deletion_window_in_days = 7
 }
+
+resource "aws_kms_alias" "a" {
+  name          = "alias/my-key-alias"
+  target_key_id = aws_kms_key.my_key.key_id
+}
