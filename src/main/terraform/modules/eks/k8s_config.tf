5b653d51bf2f72ce3282534d10c8924e63dc1e14
Brandon Domen <bdomen@google.com>
2022-08-16 19:03:35 +0000

    removing test image and adding a second target for AWS

--- src/main/terraform/modules/eks/k8s_config.tf
+++ src/main/terraform/modules/eks/k8s_config.tf
@@ -8,17 +8,17 @@ cat ${var.path_to_secrets}/*_root.pem > ${var.path_to_secrets}/all_root_certs.pe
   }
 }
 
-resource "null_resource" "create_k8s_secrets2" {
+resource "null_resource" "create_k8s_secrets" {
   provisioner "local-exec" {
     command = <<EOF
 echo "secretGenerator:" > ${var.path_to_secrets}/kustomization.yaml
 echo "- name: certs-and-configs" >> ${var.path_to_secrets}/kustomization.yaml
 echo "  Files:" >> ${var.path_to_secrets}/kustomization.yaml
-#echo "  - trusted_certs.pem" >> ${var.path_to_secrets}/kustomization.yaml
+echo "  - trusted_certs.pem" >> ${var.path_to_secrets}/kustomization.yaml
 echo "  - edp1_tls.pem" >> ${var.path_to_secrets}/kustomization.yaml
 echo "  - edp1_tls.key" >> ${var.path_to_secrets}/kustomization.yaml
 
-aws eks update-kubeconfig --region us-west-1 --name tftest-cluster
+aws eks update-kubeconfig --region ${data.aws_region.current.name} --name tftest-cluster
 
 kubectl apply -k ${var.path_to_secrets}
     EOF
