13fb7abc439e640a2bdd76541f30d3235b5955d2
Brandon Domen <bdomen@google.com>
2022-08-22 20:42:47 +0000

    small refactor

--- src/main/terraform/main.tf
+++ src/main/terraform/main.tf
@@ -15,18 +15,13 @@ module "other_resources" {
 }
 
 module "docker_config" {
-  for_each = toset(["example-panel-exchange-daemon"])
-  depends_on = [
-    module.aws_eks_cluster,
-    module.other_resources
-  ]
   source = "./modules/docker"
 
   use_test_secrets = true
   image_name = "push_aws_example_daemon_image"
   build_target_name = "example_edp_daemon_aws"
   manifest_name = "example_edp_daemon_aws.yaml"
-  repository_name = each.key
+  repository_name = "example-panel-exchange-daemon"
   path_to_secrets = "../k8s/testing/secretfiles"
   k8s_account_service_name = "edp-workflow"
   cluster_name = module.aws_eks_cluster.cluster_name
